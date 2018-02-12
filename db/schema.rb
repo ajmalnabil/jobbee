4e01c4f063b1e0556fac2d6b9bacbaa3586dddab
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2018-02-12 13:23:16 +0800

    Reinitialize repository

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206082322) do
-
-  create_table "jobs", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "image_url"
-    t.decimal "salary", precision: 9, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
